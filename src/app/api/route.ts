6398206afec120b12b607b594ee8efb3fe633be2
VincentCarrancho <71295634+VincentCarrancho@users.noreply.github.com>
2024-04-30 23:23:35 -0400

    rate limiting

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,25 +1,50 @@
 import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
+import { Ratelimit, RatelimitConfig } from "@upstash/ratelimit";
+import { kv } from "@vercel/kv";
+
+const config: RatelimitConfig = {
+  redis: kv,
+  limiter: Ratelimit.slidingWindow(5, "60s"),
+};
+
+const rateLimit = new Ratelimit(config);
 
 export async function GET(req: NextRequest) {
-  const query = req.nextUrl.searchParams.get("query");
+  const ip = req.ip ?? "127.0.0.1";
 
-  const res = await axios({
-    method: "get",
-    url: process.env.AZURE_FUNCTION_URL,
-    params: {
-      code: process.env.AZURE_FUNCTION_NEXT_APP_CODE_PARAM,
-      query: query,
-    },
-  });
+  const { limit, reset, remaining } = await rateLimit.limit(ip);
 
-  if (res.status == 200) {
-    return NextResponse.json({ message: res.data });
-  } else if (res.status >= 500) {
+  if (remaining == 0) {
     return NextResponse.json({
-      message: "Internal Server Error",
-      error: res.data,
+      error: "Rate limit exceeded. Please try again later.",
+      status: 429,
+      headers: {
+        "X-RateLimit-Limit": limit.toString(),
+        "X-RateLimit-Remaining": remaining.toString(),
+        "X-RateLimit-Reset": reset.toString(),
+      },
     });
+  } else {
+    const query = req.nextUrl.searchParams.get("query");
+
+    const res = await axios({
+      method: "get",
+      url: process.env.AZURE_FUNCTION_URL,
+      params: {
+        code: process.env.AZURE_FUNCTION_NEXT_APP_CODE_PARAM,
+        query: query,
+      },
+    });
+
+    if (res.status == 200) {
+      return NextResponse.json({ message: res.data });
+    } else if (res.status >= 500) {
+      return NextResponse.json({
+        message: "Internal Server Error",
+        error: res.data,
+      });
+    }
+    //
   }
-  //
 }
