3b05364c2f6dfaa473ffec2d4d239d0c7d3e1869
VincentCarrancho <71295634+VincentCarrancho@users.noreply.github.com>
2024-05-06 23:01:49 -0400

    update projects

--- src/app/projects/_project_component/projectcard.tsx
+++ src/app/projects/_project_component/projectcard.tsx
@@ -1,66 +1,56 @@
-"use client";
-
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-} from "@/components/ui/card";
-import { motion } from "framer-motion";
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Briefcase, ExternalLink, Lock } from "lucide-react";
 import React from "react";
 
-import { ExternalLink } from "lucide-react";
-import { string } from "zod";
-
-type ProjectCardProps = {
-  title: string;
-  description: string;
-  date_range: string[];
-  link: string;
-  tech: string[];
-};
-
 function ProjectCard({
-  title,
-  description,
-  date_range,
-  link,
-  tech,
-}: ProjectCardProps) {
+  children,
+  href,
+  name,
+  locked,
+}: {
+  children?: React.ReactNode;
+  href?: string;
+  name?: string;
+  locked: boolean;
+}) {
   return (
-    <Card className="flex flex-row justify-between">
-      <div>
-        <CardHeader className="p-4 w-full">
-          <div className="flex justify-between">
-            <div className="flex">
-              <h2 className="m-0 underline  underline-offset-4 pb-2">
-                <a href={`${link}`}>{title}</a>
-              </h2>
-            </div>
-            <div className="text-sm italic opacity-50">
-              {date_range[0]} to {date_range[1]}
-            </div>
-          </div>
-
-          <div>
-            <div className="flex space-x-2">
-              {tech.map((t, i) => (
-                <motion.div
-                  whileHover={{ scale: 1.05 }}
-                  whileTap={{ scale: 0.975 }}
-                  key={t + `-${i}`}
-                >
-                  <Badge>{t}</Badge>
-                </motion.div>
-              ))}
-            </div>
-          </div>
-        </CardHeader>
-        <CardContent>{description}</CardContent>
-      </div>
-    </Card>
+    <Accordion type="single" collapsible>
+      <AccordionItem value={name + "accordion"}>
+        <AccordionTrigger className="m-0 p-0" disabled={locked}>
+          <h2 className="m-0 p-0">
+            <code className={`flex space-x-4 m-0 p-0 items-center`}>
+              {locked ? (
+                <span className="opacity-30 ">{name}</span>
+              ) : (
+                <span>{name}</span>
+              )}
+              {locked && <Lock className="opacity-30 " />}
+            </code>
+          </h2>
+        </AccordionTrigger>
+        <AccordionContent>
+          {href && (
+            <a
+              href={href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center space-x-2 underline underline-offset-4 italic"
+            >
+              <span className="font-bold">Link to work</span>
+              <span>
+                <ExternalLink size={16} />
+              </span>
+            </a>
+          )}
+          <div className="border-l-2 pl-6">{children}</div>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   );
 }
 
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,163 +6,218 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { ExternalLink } from "lucide-react";
 
 function ProjectsPage() {
   return (
     <div>
       <h1>My Projects and Related Experience</h1>
-      <h2>
-        <code>Under Construction 🚧 🚧 🚧</code>
-      </h2>
-      <div className="pb-10">
-        You can still ask about my projects{" "}
-        <a href="/" className="underline underline-offset-4">
-          here
-        </a>{" "}
-        in my AMA section!
-      </div>
-      <div className="opacity-30 pointer-events-none border border-dashed rounded-2xl p-4">
-        <div>These are some of the projects I have worked on in the past. </div>
-        <Accordion type="single" collapsible disabled>
-          <AccordionItem value="item-1">
-            <AccordionTrigger className="m-0 p-0">
-              <h1>2024</h1>
-            </AccordionTrigger>
-            <AccordionContent className="space-y-4">
-              <ProjectCard
-                title="STARS GPT"
-                date_range={["02/2024", "04/2024"]}
-                description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, doloremque, molestiae sapiente eum, repudiandae iusto esse quae vero aspernatur nesciunt totam laboriosam? Nulla iure excepturi inventore? Vero beatae quae modi."
-                link="https://stars-gpt-frontend-testing.onrender.com/"
-                tech={[
-                  "Next.js",
-                  "TailwindCSS",
-                  "TypeScript",
-                  "FastAPI",
-                  "Langchain",
-                ]}
-              />
-              <ProjectCard
-                title="Arthas AI"
-                date_range={["02/2024", "04/2024"]}
-                description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, doloremque, molestiae sapiente eum, repudiandae iusto esse quae vero aspernatur nesciunt totam laboriosam? Nulla iure excepturi inventore? Vero beatae quae modi."
-                link="https://stars-gpt-frontend-testing.onrender.com/"
-                tech={[
-                  "Next.js",
-                  "TailwindCSS",
-                  "TypeScript",
-                  "FastAPI",
-                  "Langchain",
-                ]}
-              />
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-        <Accordion type="single" collapsible disabled>
-          <AccordionItem value="item-1">
-            <AccordionTrigger className="m-0 p-0">
-              <h1>2023</h1>
-            </AccordionTrigger>
-            <AccordionContent className="space-y-4">
-              <ProjectCard
-                title="STARS GPT"
-                date_range={["02/2024", "04/2024"]}
-                description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, doloremque, molestiae sapiente eum, repudiandae iusto esse quae vero aspernatur nesciunt totam laboriosam? Nulla iure excepturi inventore? Vero beatae quae modi."
-                link="https://stars-gpt-frontend-testing.onrender.com/"
-                tech={[
-                  "Next.js",
-                  "TailwindCSS",
-                  "TypeScript",
-                  "FastAPI",
-                  "Langchain",
-                ]}
-              />
-              <ProjectCard
-                title="Arthas AI"
-                date_range={["02/2024", "04/2024"]}
-                description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, doloremque, molestiae sapiente eum, repudiandae iusto esse quae vero aspernatur nesciunt totam laboriosam? Nulla iure excepturi inventore? Vero beatae quae modi."
-                link="https://stars-gpt-frontend-testing.onrender.com/"
-                tech={[
-                  "Next.js",
-                  "TailwindCSS",
-                  "TypeScript",
-                  "FastAPI",
-                  "Langchain",
-                ]}
-              />
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-        <Accordion type="single" collapsible disabled>
-          <AccordionItem value="item-1">
-            <AccordionTrigger className="m-0 p-0">
-              <h1>2022</h1>
-            </AccordionTrigger>
-            <AccordionContent className="space-y-4">
-              <ProjectCard
-                title="STARS GPT"
-                date_range={["02/2024", "04/2024"]}
-                description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, doloremque, molestiae sapiente eum, repudiandae iusto esse quae vero aspernatur nesciunt totam laboriosam? Nulla iure excepturi inventore? Vero beatae quae modi."
-                link="https://stars-gpt-frontend-testing.onrender.com/"
-                tech={[
-                  "Next.js",
-                  "TailwindCSS",
-                  "TypeScript",
-                  "FastAPI",
-                  "Langchain",
-                ]}
-              />
-              <ProjectCard
-                title="Arthas AI"
-                date_range={["02/2024", "04/2024"]}
-                description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, doloremque, molestiae sapiente eum, repudiandae iusto esse quae vero aspernatur nesciunt totam laboriosam? Nulla iure excepturi inventore? Vero beatae quae modi."
-                link="https://stars-gpt-frontend-testing.onrender.com/"
-                tech={[
-                  "Next.js",
-                  "TailwindCSS",
-                  "TypeScript",
-                  "FastAPI",
-                  "Langchain",
-                ]}
-              />
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-        <Accordion type="single" collapsible disabled>
-          <AccordionItem value="item-1">
-            <AccordionTrigger className="m-0 p-0">
-              <h1>2020-2021</h1>
-            </AccordionTrigger>
-            <AccordionContent className="space-y-4">
-              <ProjectCard
-                title="STARS GPT"
-                date_range={["02/2024", "04/2024"]}
-                description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, doloremque, molestiae sapiente eum, repudiandae iusto esse quae vero aspernatur nesciunt totam laboriosam? Nulla iure excepturi inventore? Vero beatae quae modi."
-                link="https://stars-gpt-frontend-testing.onrender.com/"
-                tech={[
-                  "Next.js",
-                  "TailwindCSS",
-                  "TypeScript",
-                  "FastAPI",
-                  "Langchain",
-                ]}
-              />
-              <ProjectCard
-                title="Arthas AI"
-                date_range={["02/2024", "04/2024"]}
-                description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, doloremque, molestiae sapiente eum, repudiandae iusto esse quae vero aspernatur nesciunt totam laboriosam? Nulla iure excepturi inventore? Vero beatae quae modi."
-                link="https://stars-gpt-frontend-testing.onrender.com/"
-                tech={[
-                  "Next.js",
-                  "TailwindCSS",
-                  "TypeScript",
-                  "FastAPI",
-                  "Langchain",
-                ]}
-              />
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
+
+      <div>
+        <section>
+          <h1>Work Experience</h1>
+          <ProjectCard
+            locked={false}
+            name={"Salesforce Summer '23"}
+            href="https://help.salesforce.com/s/articleView?id=release-notes.rn_security_pe_key_management_accessibility.htm&release=246&type=5"
+          >
+            <h3>Main Accomplishment</h3>
+            <div>
+              {`Improved the accessibility and UI of Salesforce\'s web application
+              and impacted over 100+ customers of this service such as Apple,
+              Uber, Mariott, etc.`}
+            </div>
+            <h3>What I did</h3>
+            <div>
+              <ul>
+                <li>
+                  Spearheaded development of web pages to empower customers in
+                  managing encryption keys (data security).
+                </li>
+                <li>
+                  Improved Salesforce web app accessibility by fixing 4 issues
+                  through UI framework migration (Java, Javascript, HTML, CSS).
+                </li>
+                <li>
+                  Collaborated with engineers to create robust code and
+                  documented UI migration process for future reference.
+                </li>
+                <li>
+                  Addressed accessibility bugs (resizing errors, UI behavior) to
+                  improve user experience.
+                </li>
+                <li>
+                  Migrated Salesforce UI to Lightning Design System and Aura
+                  framework for a modern, user-friendly interface.
+                </li>
+                <li>
+                  Refined design and functionality of specific pages through UI
+                  rework initiatives (feedback, iteration, implementation).{" "}
+                </li>
+                <li>
+                  Contributed to changes deployed in Salesforce Winter ’24
+                  release, demonstrating impact on platform development.
+                </li>
+              </ul>
+            </div>
+          </ProjectCard>
+          <ProjectCard locked={false} name={"MetLife Summer '22"}>
+            <h3>Main Accomplishment</h3>
+            <div>
+              Upgraded a 10+ year old application and converted it to a modern
+              web application that showed over 40% increase in runtime
+              performance.
+            </div>
+            <h3>What I did</h3>
+            <div>
+              <ul>
+                <li>
+                  Developed full-stack application for visualizing corporate
+                  billing reports at MetLife.
+                </li>
+                <li>
+                  Migrated legacy, slow Microsoft Excel script (30+ seconds
+                  runtime) to a faster Java-based application.{" "}
+                </li>
+                <li>
+                  Reduced application runtime by 40% through code optimization
+                  and leveraging async/multithreaded nature of Java.
+                </li>
+                <li>
+                  Built Spring Boot REST API to retrieve and analyze data from
+                  IBM DB2 SQL Database.
+                </li>
+                <li>
+                  Utilized React for a performant frontend with pagination to
+                  prevent data overload and improve user experience.
+                </li>
+              </ul>
+            </div>
+          </ProjectCard>
+        </section>
+        <section>
+          <h1>Projects</h1>
+          <ProjectCard
+            locked={false}
+            name={"STARS GPT Spring '24"}
+            href="https://stars-gpt-frontend-testing.onrender.com/"
+          >
+            <h3>Description</h3>
+            <div>
+              {`Led a team of 5 developers in creating a fullstack web application for my school\’s tutoring group. The application is a tutoring chatbot designed to help students and tutors with their education. This project uses Langchain, OpenAI API, FastAPI, Supabase, React (using Vite as the build tool), docker, and Render.com to deploy (we successfully used AWS EC2 and Azure VMs but settled on using Render's container hosting).`}
+            </div>
+            <h3>{`Here\'s what I did:`}</h3>
+            <div>
+              <ul>
+                <li>
+                  Created system architecture and design for the application
+                  including the database schema, API endpoints, and frontend
+                  components.
+                </li>
+                <li>
+                  Developed the frontend with React using ShadCN for as the
+                  design system for the application.
+                </li>
+                <li>
+                  Implemented the Langchain and OpenAI Logic for the chatbot API
+                  functionality.
+                </li>
+                <li>
+                  Implemented a Singleton model to reduce costs everytime the
+                  chatbot response is invoked by the user.
+                </li>
+                <li>
+                  {`Deployed backend API on multiple cloud services such as AWS
+                  EC2, Azure VMs, and Docker Containers (ended up using
+                  Render.com\'s container hosting).`}
+                </li>
+              </ul>
+            </div>
+          </ProjectCard>
+          <ProjectCard
+            locked={true}
+            name={"Arthas AI"}
+            href="https://help.salesforce.com/s/articleView?id=release-notes.rn_security_pe_key_management_accessibility.htm&release=246&type=5"
+          >
+            <h3>{`Here\'s what I did:`}</h3>
+            <div>
+              <ul>
+                <li>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Quaerat, accusamus!
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Doloremque, vero explicabo.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Explicabo doloremque incidunt animi alias perferendis.
+                </li>
+              </ul>
+            </div>
+          </ProjectCard>
+          <ProjectCard name={"ChaRizzMa: Dating Tracker"} locked={true}>
+            <h3>{`Here\'s what I did:`}</h3>
+            <div>
+              <ul>
+                <li>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Quaerat, accusamus!
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Doloremque, vero explicabo.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Explicabo doloremque incidunt animi alias perferendis.
+                </li>
+              </ul>
+            </div>
+          </ProjectCard>
+          <ProjectCard name={"Hotel iPad Check-In System"} locked={true}>
+            <h3>{`Here\'s what I did:`}</h3>
+            <div>
+              <ul>
+                <li>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Quaerat, accusamus!
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Doloremque, vero explicabo.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Explicabo doloremque incidunt animi alias perferendis.
+                </li>
+              </ul>
+            </div>
+          </ProjectCard>
+          <ProjectCard
+            name={"FIU Panthers Protecting Panthers Prototype"}
+            locked={true}
+          >
+            <h3>{`Here\'s what I did:`}</h3>
+            <div>
+              <ul>
+                <li>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Quaerat, accusamus!
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Doloremque, vero explicabo.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Explicabo doloremque incidunt animi alias perferendis.
+                </li>
+              </ul>
+            </div>
+          </ProjectCard>
+        </section>
       </div>
     </div>
   );
